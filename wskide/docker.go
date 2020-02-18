aabf50f882ef0d5d8cdda6f0f034e28ad0696637
Michele Sciabarra <nimmichele@sciabarra.com>
2020-02-18 16:30:18 +0100

    add status command

--- wskide/docker.go
+++ wskide/docker.go
@@ -3,3 +3,11 @@ package wskide
 func dockerVersion() string {
 	return Sys("@docker version --format {{.Server.Version}}")
 }
+
+func dockerStatus() (err error) {
+	err = Run("docker ps -a")
+	if err != nil {
+		return err
+	}
+	return nil
+}
