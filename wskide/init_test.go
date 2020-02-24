22fa877c10fce42205088a22823b001de18a7afd
Michele Sciabarra <msciabarra@apache.org>
2020-02-24 11:22:27 +0100

    preparing for release 0.3

--- wskide/init_test.go
+++ wskide/init_test.go
@@ -2,17 +2,25 @@ package wskide
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
 )
 
 func ExampleInit() {
 	*DryRunFlag = false
-	//DryRunPush()
 	Sys("rm -Rf /tmp/io-sdk/demo")
-	fmt.Println(Init("/tmp/io-sdk/demo", "javascript"))
-	fmt.Println(Init("/tmp/io-sdk/non-exists-directory", "non-exists-language"))
-	fmt.Println(Init("/tmp/io-sdk/demo", "javascript"))
+	os.Setenv("HOME", "/tmp")
+	fmt.Println(0, Init("/var/tmp/demo", "javascript", ioutil.Discard))
+	fmt.Println(1, Init("/tmp/io-sdk/demo", "javascript", ioutil.Discard))
+	fmt.Println(2, Init("/tmp/io-sdk/non-exists-directory", "non-exists-language", ioutil.Discard))
+	fmt.Println(3, Init("/tmp/io-sdk/demo", "javascript", ioutil.Discard))
 	// Output:
-	// <nil>
-	// authentication required
-	// repository already exists
+	// 0 work directory /var/tmp/demo should be under your home directory; this is required to be accessible by Docker
+	// Preparing work directory /tmp/io-sdk/demo for javascript
+	// Done.
+	// 1 <nil>
+	// Preparing work directory /tmp/io-sdk/non-exists-directory for non-exists-language
+	// 2 authentication required
+	// Preparing work directory /tmp/io-sdk/demo for javascript
+	// 3 repository already exists
 }
