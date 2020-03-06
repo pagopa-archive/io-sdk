bbb308023c308a0cba693c0942c0f4a3c3e63031
Michele Sciabarra <nimmichele@sciabarra.com>
2020-03-06 17:37:59 +0100

    send messages from frontend

--- backend/actions/src/send/const.go
+++ backend/actions/src/send/const.go
@@ -1,7 +1,10 @@
 package main
 
+const apihost = "https://api.cd.italia.it/api/v1"
+
 const answer = `<table>
  <tr><th>CodFiscDesc</th><td>{{.CodFiscDest}}</td></td>
+ <tr><th>MessageSubject</th><td>{{.MessageSubject}}</td></td>
  <tr><th>Message</th><td>{{.Message}}</td></td>
 </table>
 `
