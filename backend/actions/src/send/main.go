c6c1b65d9a5839cc93696bd083b93bd9021ba18b
nimmichele <57905158+nimmichele@users.noreply.github.com>
2020-03-12 12:42:13 +0100

    Send messages (#26)
    * send messages from frontend

--- backend/actions/src/send/main.go
+++ backend/actions/src/send/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"html/template"
 )
 
@@ -10,6 +11,14 @@ func Main(data map[string]interface{}) map[string]interface{} {
 	t := template.Must(template.New("answer").Parse(answer))
 	buf := new(bytes.Buffer)
 	t.Execute(buf, data)
+	fmt.Println(data)
+
+	// MessageSubject := fmt.Sprintf("%v", data["MessageSubject"])
+	// MessageSubject = "Welcome new user!"
+	// Message := fmt.Sprintf("%v", data["Message"])
+	// CodFiscDest := fmt.Sprintf("%v", data["CodFiscDest"])
+	// APIKeyIO := fmt.Sprintf("%v", data["ApiKeyIO"])
+	// SendMessage(MessageSubject, Message, CodFiscDest, APIKeyIO)
 
 	// result
 	res := make(map[string]interface{})
--- backend/actions/src/send/send_test.go
+++ backend/actions/src/send/send_test.go
@@ -4,13 +4,13 @@ import "fmt"
 
 func ExampleSendMessage() {
 	fmt.Println("test disabled - need to store secrets in a safe way")
-	return
+	//return
 	var subject = "Welcome new user !"
 	var markdown = `# This is a markdown header\n\nto show how easily markdown can be converted to **HTML**\n\nRemember: this has to be a long text.`
 	// need this for the Example!!
 	// markdown = strings.Replace(markdown, "\n", `\n`, -1)
 	var dest = "ISPXNB32R82Y766D"
-	var key = "XXXXX"
+	var key = "483b7b1f3a974b45b5c44a43538c9255"
 	SendMessage(subject, markdown, dest, key)
 	// response Body: {"id":"01E1FHC33GSY6CE64JTGDYTYMP"}
 
