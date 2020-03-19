588ebe0019e8ece52e5b4527b1a7f67dce26797a
Michele Sciabarra <michele@sciabarra.com>
2020-03-19 16:04:49 +0100

    add command config: ask apikey and write to $HOME/.iosdk

---
+++ wskide/const.go
@@ -0,0 +1,16 @@
+package wskide
+
+//MinDockerVersion required
+const MinDockerVersion = "18.06.3-ce"
+
+// BrowserURL to access
+const BrowserURL = "http://localhost:3280/"
+
+// IdeJsImage is the image for the ide
+const IdeJsImage = "actionloop/ide-js:latest"
+
+// OpenwhiskStandaloneImage is the image for the standalone openwhisk
+const OpenwhiskStandaloneImage = "actionloop/iosdk:latest"
+
+// APIHOST to send messages
+const APIHOST = "https://api.cd.italia.it/api/v1"
