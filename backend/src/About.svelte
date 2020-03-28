bdbdf0288e52078d114bf388282a7bdefb40f920
Michele Sciabarra <msciabarra@apache.org>
2020-03-28 11:38:30 +0100

    reworked menus

--- backend/src/About.svelte
+++ backend/src/About.svelte
@@ -4,12 +4,14 @@ import marked from 'marked';
 let body = marked(`
 # Benvenuto ad IO-SDK
 
-IO-SDK è il kit di sviluppo per sviluppare Application per la app dei servizi pubblici italiani.
+IO-SDK è il kit di sviluppo per IO.
+
+Consente lo sviluppo di importatori per la app dei servizi pubblici italiani.
 
 Il nostro team è composto da:
 
 - Federico Feroldi, Team Leader
-- Mirko Calvaresi, Projct Manager
+- Mirko Calvaresi, Project Manager
 - Michele Sciabarrà, Developer
 `);
 </script>
--- backend/src/MenuItem.svelte
+++ backend/src/MenuItem.svelte
@@ -1,19 +1,32 @@
 <script>
   import { Link } from "svelte-routing";
- 
+
   export let icon = "fas fa-home";
   export let description = "Home";
   export let to = "/";
+  export let url = "";
 
   function getProps(args) {
-      return { 
-          class: "large list-item" 
-      }
+    return {
+      class: "large list-item"
+    };
+  }
+  function visit(event) {
+    event.preventDefault()
+    window.open(url, to)
   }
 </script>
 
 <li>
-    <Link {to} getProps="{getProps}">
-      <i class={icon} /> {description}
+  {#if url == ''}
+    <Link {to} {getProps}>
+      <i class={icon} />
+      {description}
     </Link>
+  {:else}
+    <a href={url} class="large list-item" rel="noopener noreferrer" on:click={visit}>
+      <i class={icon} />
+      {description}
+    </a>
+  {/if}
 </li>
