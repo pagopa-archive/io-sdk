16132981927008f8ab8004292536a35fa4bfa86e
Michele Sciabarra <msciabarra@apache.org>
2020-04-17 16:29:11 +0200

    including nvm in test

--- test.sh
+++ test.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+set -e
+source $HOME/.nvm/nvm.sh
+nvm use v10
 iosdk/iosdk stop
 rm -Rvf $HOME/tmp-iosdk-test
 iosdk/iosdk init $HOME/tmp-iosdk-test pagopa/io-sdk-javascript --io-apikey=123456890 --wskprops
