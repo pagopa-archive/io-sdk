fb3718410741669a158211d6d6cd56c9369a0704
Michele Sciabarra <msciabarra@apache.org>
2020-04-17 14:41:27 +0200

    fix input not a tty

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ rm -Rvf $HOME/tmp-iosdk-test
 iosdk/iosdk init $HOME/tmp-iosdk-test pagopa/io-sdk-javascript --io-apikey=123456890 --wskprops
 docker pull library/redis:5
 iosdk/iosdk -v start --skip-pull-images --skip-docker-version --skip-open-browser
-docker exec -ti iosdk-theia bash /home/project/build.sh
+docker exec iosdk-theia bash /home/project/build.sh
 CHECK=ISPXNB32R82Y766F
 DATA="${1:-$HOME/tmp-iosdk-test/data/data.xlsx}"
 URL="http://localhost:3280/api/v1/web/guest/iosdk/import"
