6eb2fe7d75b1a6e0689696a0e385c63d5ee018ec
Michele Sciabarra <msciabarra@apache.org>
2020-06-10 16:37:00 +0200

    ignore stdin for httpie

--- admin/actions/test/util.bash
+++ admin/actions/test/util.bash
@@ -26,24 +26,24 @@ function setup {
 
 function get { 
   echo ">>> " python3 -m httpie --timeout=300 GET "$@" >>/tmp/debug.log
-  run python3 -m httpie --timeout=300 GET "$@"
+  run python3 -m httpie --ignore-stdin --timeout=300 GET "$@"
   echo "$output" >>/tmp/debug.log
 }
 
 function put { 
   echo ">>> " python3 -m httpie --timeout=300 PUT "$@" >>/tmp/debug.log
-  run python3 -m httpie --timeout=300 PUT "$@"
+  run python3 -m httpie --ignore-stdin --timeout=300 PUT "$@"
   echo "$output" >>/tmp/debug.log
 }
 
 function post { 
-  echo ">>> " python3 -m httpie --timeout=300 POST "$@" >>/tmp/debug.log
+  echo ">>> " python3 -m httpie --ignore-stdin --timeout=300 POST "$@" >>/tmp/debug.log
   run python3 -m httpie --timeout=300 POST "$@"
   echo "$output" >>/tmp/debug.log
 }
 
 function fpost { 
-  echo ">>> " python3 -m httpie -f --timeout=300 POST "$@" >>/tmp/debug.log
+  echo ">>> " python3 -m httpie -f --ignore-stdin --timeout=300 POST "$@" >>/tmp/debug.log
   run python3 -m httpie -f --timeout=300 POST "$@"
   echo "$output" >>/tmp/debug.log
 }
--- admin/actions/venv.sh
+++ admin/actions/venv.sh
@@ -3,4 +3,4 @@ virtualenv virtualenv
 source virtualenv/bin/activate
 pip3 install redis
 apt-get update && apt-get -y install zip
-zip -r /mnt/virtualenv.zip virtualenv
+zip -q -r /mnt/virtualenv.zip virtualenv
