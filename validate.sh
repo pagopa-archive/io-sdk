c392401c90292b2448b0b6c59239773b16154313
Michele Sciabarra <msciabarra@apache.org>
2020-06-19 12:13:15 +0200

    validate for mac

---
+++ validate.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+VERSION=$1
+# pick latest snapshot
+if test -z "$VERSION"
+then VERSION=$(git tag | grep snapshot | sort -r | head -1)
+     echo validating latest snapshot: $VERSION
+fi
+# cleanup
+set -e
+# install
+PKG=IO-SDK-macos-installer-x64-${VERSION}.pkg
+URL=https://github.com/pagopa/io-sdk/releases/download/${VERSION}/$PKG
+echo downloading $URL
+curl -sL $URL >/tmp/$PKG
+sudo installer -pkg /tmp/$PKG -target /
+# check version
+V=$(iosdk --version 2>&1)
+if test "$V" != "$VERSION"
+then echo $V: wrong version, expected $VERSION
+     echo FAIL
+     exit 1
+fi
+# init
+rm -Rvf $HOME/tmp-iosdk-validation
+/usr/local/bin/iosdk init $HOME/tmp-iosdk-validation pagopa/io-sdk-javascript --io-apikey=123456890
+# test
+/usr/local/bin/iosdk stop
+/usr/local/bin/iosdk -v start --skip-open-browser
+docker exec iosdk-theia bash -c 'bash /home/project/build.sh'
+/usr/local/bin/iosdk status
+CHECK=ISPXNB32R82Y766F
+DATA="${1:-$HOME/tmp-iosdk-validation/data/data.xlsx}"
+URL="http://localhost:3280/api/v1/web/guest/iosdk/import"
+JSON='{"file": "'$(base64 $DATA | tr -d '\n')'"}'
+HEAD="Content-Type: application/json"
+curl -s $URL -H "$HEAD" -d "$JSON"  | grep $CHECK >/dev/null
+RES=$?
+# cleanup
+rm -Rvf $HOME/tmp-iosdk-validation
+/usr/local/bin/iosdk stop
+yes | sudo /Library/IO-SDK/$VERSION/uninstall.sh
+# result
+if test "$RES" == "0" 
+then echo SUCCESS ; exit 0
+else echo FAIL ; exit 1
+fi
