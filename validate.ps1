f77a36a1a1a37111b0ee50c6c39ec91751ff9c3f
Michele Sciabarra <michele@sciabarra.com>
2020-08-06 12:39:46 +0200

    udpdated validate in powershell

--- validate.ps1
+++ validate.ps1
@@ -13,54 +13,49 @@
    Validation of the iosdk installation
 #>
 
-if ($args[0] -eq $null) {
+$version = "2020.0805.1627-snapshot"
+if ($null -eq $args[0]) {
     Write-Output "usage: validate.ps1 <version>"
     exit
-} 
-
+}
 $version = $args[0]
 
-
 $file = "iosdk_$version.exe"
-
 $url = "https://github.com/pagopa/io-sdk/releases/download/$version/$file"
-
-Write-Output $url
-
 $outpath = "$env:TEMP/$file"
 
 Invoke-WebRequest -Uri $url -OutFile $outpath
-
-$args = @("/S")
-Start-Process -Filepath "$outpath" -ArgumentList $args -Verb RunAs
-
-#Start-Sleep -s 5
+Start-Process -Filepath "$outpath" -ArgumentList @("/S") -Verb RunAs
 
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","User")
 
-if (Write-Output $env:Path | Select-String -Pattern iosdk) {
-    Write-Output "PATH OK"
+if (! (Write-Output $env:Path | Select-String -Pattern iosdk)) {
+    Write-Output "PATH not set properly"
+    Write-Output "FAIL"
+    exit
 }
 
-#Start-Process -NoNewWindow -Wait -Filepath "$env:ProgramFiles\IOSDK\iosdk.exe" -ArgumentList "init, $HOME/tmp-iosdk-validation, pagopa/io-sdk-javascript, --io-apikey=123456" 
 & "$env:ProgramFiles\IOSDK\iosdk.exe" "init" "$HOME/tmp-iosdk-validation" "pagopa/io-sdk-javascript" "--io-apikey=123456"
+& "$env:ProgramFiles\IOSDK\iosdk.exe" "start" "--skip-open-browser"
 
-#Start-Process -NoNewWindow -Wait -Filepath "$env:ProgramFiles\IOSDK\iosdk.exe" -ArgumentList "start"
-& "$env:ProgramFiles\IOSDK\iosdk.exe" "start"
-
-#Start-Process -NoNewWindow -Wait -Filepath "$env:ProgramFiles\IOSDK\iosdk.exe" -ArgumentList "status"
 & "$env:ProgramFiles\IOSDK\iosdk.exe" "status"
 
-#Start-Process -NoNewWindow -Wait -Filepath "$env:ProgramFiles\Docker\Docker\Resources\bin\docker.exe" -ArgumentList "exec, iosdk-theia, /home/project/build.sh"
 & "docker" "exec" "iosdk-theia" "/home/project/build.sh"
 
-#Start-Process -NoNewWindow -Wait -Filepath "$env:ProgramFiles\IOSDK\iosdk.exe" -ArgumentList "status"
-& "$env:ProgramFiles\IOSDK\iosdk.exe" "status"
+$dataFile = "$HOME/tmp-iosdk-validation/data/data.xlsx" 
+$bytes = [System.IO.File]::ReadAllBytes($dataFile)
+$encoded = [System.Convert]::ToBase64String($bytes)
+$testJson = "{`"file`": `"'$encoded`"}"
+$testUrl = "http://localhost:3280/api/v1/web/guest/iosdk/import"
 
-#Start-Process -NoNewWindow -Wait -Filepath "$env:ProgramFiles\IOSDK\iosdk.exe" -ArgumentList "stop"
-& "$env:ProgramFiles\IOSDK\iosdk.exe" "stop"
+$out = Invoke-RestMethod -Uri $testUrl -Method Post -ContentType "application/json" -Body $testJson 
+
+if($out.data[0].fiscal_code -eq "ISPXNB32R82Y766F") {
+   Write-Output "SUCCESS"
+} else {
+   Write-Output "FAIL"
+}
 
 Remove-Item -Recurse -Force "$HOME\tmp-iosdk-validation"
 
-Start-Process -Filepath "$env:ProgramFiles/IOSDK/uninstall.exe" -ArgumentList $args -Verb RunAs -Wait
-
+& Start-Process -Filepath "$env:ProgramFiles/IOSDK/uninstall.exe" -ArgumentList @("/S") -Verb RunAs -Wait
