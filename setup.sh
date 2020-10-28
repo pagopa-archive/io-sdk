9b7a34ef4911a13081423054ed4719bcc3eed2b6
Michele Sciabarra <michele@sciabarra.com>
2020-10-28 20:43:17 +0100

    adding libraries to support playwright in github actions

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
-
 export NODENV_VERSION=12.18.0
 export PYENV_VERSION=3.7.7
 export GOENV_VERSION=1.13.12
@@ -17,10 +16,22 @@ case "$(uname)" in
         unzip -o $BIN/wsk.zip wsk -d $BIN 
     ;;
     (Linux)
-	sudo apt-get update
+	    sudo apt-get update
         sudo apt-get install -y build-essential libssl-dev zlib1g-dev libbz2-dev \
             libreadline-dev libsqlite3-dev wget curl libncurses5-dev libncursesw5-dev \
             xz-utils  libffi-dev liblzma-dev python-openssl git zip
+        # libraries required to run browsers and support playwright
+        sudo apt-get install -y gstreamer1.0-libav gstreamer1.0-plugins-bad \
+            gstreamer1.0-plugins-base gstreamer1.0-plugins-good libasound2 libatk-bridge2.0-0 \
+            libatk1.0-0 libatspi2.0-0 libbrotli1 libcairo-gobject2 libcairo2 libcups2 libdbus-1-3 \
+            libdbus-glib-1-2 libdrm2 libegl1 libenchant1c2a libepoxy0 libfontconfig1 libfreetype6 \
+            libgbm1 libgdk-pixbuf2.0-0 libgl1 libgles2 libglib2.0-0 libgstreamer-gl1.0-0 \
+            libgstreamer1.0-0 libgtk-3-0 libgtk2.0-0 libharfbuzz-icu0 libharfbuzz0b libhyphen0 \
+            libicu60 libjpeg-turbo8 libnotify4 libnspr4 libnss3 libopenjp2-7 libopus0 libpango-1.0-0 \
+            libpangocairo-1.0-0 libpangoft2-1.0-0 libpng16-16 libsecret-1-0 libvpx5 libwayland-client0 \
+            libwayland-egl1 libwayland-server0 libwebp6 libwebpdemux2 libwoff1 libx11-6 libx11-xcb1 \
+            libxcb-dri3-0 libxcb-shm0 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 \
+            libxi6 libxkbcommon0 libxml2 libxrandr2 libxrender1 libxslt1.1 libxt6 libxtst6
         WSK_INSTALL=https://github.com/apache/openwhisk-cli/releases/download/1.0.0/OpenWhisk_CLI-1.0.0-linux-amd64.tgz
         JQ_INSTALL=https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
         curl -sL $WSK_INSTALL >$BIN/wsk.tgz
