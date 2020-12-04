46399f90255a0ace5f2700c70968afad3131768d
sf3ris <vale.piz92@hotmail.it>
2020-12-04 09:57:10 +0100

    Implemented Form Validation (#100)
    * WIP: Implemented Form Validation
    
    * Adapted POST data for backend action
    
    * Fixed  button not resetting form properly
    
    * Added UI Validation on Form's Input fields

--- admin/web/src/Send.svelte
+++ admin/web/src/Send.svelte
@@ -1,24 +1,51 @@
 <script>
-  import { onMount } from "svelte";
- 
-  export let api;
   export let key = "";
-  
+
+  import { onMount } from "svelte";
+  import { formValidator } from './lib/validator/formValidator';
+
+  let base = "http://localhost:3280/api/v1/web/guest/";
+
   let state = {};
-  let action = "/util/send";
+  let action = "util/send";
+  let isFormValid = false;
   let data = {
-    fiscal_code: "",
-    subject: "",
-    markdown: "",
-    amount: "",
-    notice_number: ""
+    fiscal_code: {
+      value: "",
+      rules: "required|fiscalCode"
+    },
+    subject: {
+      value: "",
+      rules: "required|min:5|max:255"
+    },
+    markdown: {
+      value: "",
+      rules: "required"
+    },
+    amount: {
+      value: "",
+      rules: "required|numeric"
+    },
+    notice_number: {
+      value: "",
+      rules: "required"
+    }
   };
+
+  function validateForm( ) {
+
+    const res = formValidator.validateForm( data );
+
+    data        = res.validatedData;
+    isFormValid = res.isFormValid;
+
+  }
 
   async function start() {
     if (key == "") return;
     let id = window.atob(key);
     console.log("start");
-    fetch(api + "/util/cache", {
+    fetch(base + "util/cache", {
       method: "POST",
       body: JSON.stringify({ get: id }),
       headers: { "Content-Type": "application/json" }
@@ -38,14 +65,33 @@
         state = { error: err };
       });
   }
-  onMount(start);
+  
+  onMount(() => {
+    start();
+    validateForm();
+  });
+
+  function formatData( data ) {
+
+    let formattedData = {};
+
+    Object.keys(data).map((key, index) => {
+
+        formattedData[key] = data[key].value;
+
+    })
+
+    return formattedData;
+
+  }
 
   function submitForm() {
-    let url = api + action;
-    console.log(url);
+
+    let url = base + action;
+    
     fetch(url, {
       method: "POST",
-      body: JSON.stringify(data),
+      body: JSON.stringify(formatData(data)),
       headers: {
         "Content-Type": "application/json"
       }
@@ -59,12 +105,28 @@
   }
 
   function resetForm() {
-    result = "";
-    error = "";
-    for (key in data) {
-      data[key] = "";
-    }
+
+    state.result = "";
+    state.error = "";
+
+    Object.keys(data).forEach((key, index) => {
+
+      data[key].value = "";
+
+    });
+
+    validateForm();
+
   }
+
+  function onChangeFieldValue(field, value) {
+
+    data[field].value = value;
+
+    validateForm( );
+
+  }
+
 </script>
 
 <h2>Send a Single Message</h2>
@@ -75,67 +137,72 @@
   </div>
   <div>
     <button type="button" class="btn btn-primary" on:click={resetForm}>
-      New Message
+      Nuovo Messaggio
     </button>
   </div>
 {:else if state.error}
   <div class="alert alert-danger" role="alert">{state.error}</div>
   <div>
     <button type="button" class="btn btn-primary" on:click={resetForm}>
-      New Message
+      Nuovo Messaggio
     </button>
   </div>
 {:else}
   <div>
     <div class="form-group">
-      <label class="active" for="fiscal_code">
-        Fiscal Code
-      </label>
-      <input
-        type="text"
-        class="form-control"
-        id="fiscal_code"
-        bind:value={data.fiscal_code} />
+
+        <label class="active" for="fiscal_code">
+          Fiscal Code
+        </label>
+        <input
+          type="text"
+          class={data.fiscal_code.valid === true ? 'form-control is-valid' : 'form-control is-invalid'}
+          id="fiscal_code"
+          on:input={e => onChangeFieldValue('fiscal_code', e.target.value) } />
+
     </div>
     <div class="form-group">
       <label class="active" for="subject">Subject</label>
       <input
         type="text"
-        class="form-control"
+        class={data.subject.valid === true ? 'form-control is-valid' : 'form-control is-invalid'}
         id="subject"
-        bind:value={data.subject} />
+        on:input={e => onChangeFieldValue('subject', e.target.value) } />
     </div>
     <div class="form-group">
-      <textarea id="markdown" rows="3" bind:value={data.markdown} />
+      <textarea 
+        id="markdown" 
+        rows="3" 
+        on:input={e => onChangeFieldValue('markdown', e.target.value) } />
       <label class="active" for="markdown">Message (markdown)</label>
     </div>
     <div class="form-group">
       <label class="active" for="amount">Amount</label>
       <input
         type="text"
-        class="form-control"
+        class={data.amount.valid === true ? 'form-control is-valid' : 'form-control is-invalid'}
         id="amount"
-        bind:value={data.amount} />
+        on:input={e => onChangeFieldValue('amount', e.target.value) } />
     </div>
     <div class="form-group">
       <label class="active" for="amount">Notice Number</label>
       <input
         type="text"
-        class="form-control"
+        class={data.notice_number.valid === true ? 'form-control is-valid' : 'form-control is-invalid'}
         id="notice_number"
-        bind:value={data.notice_number} />
+        on:input={e => onChangeFieldValue('notice_number', e.target.value) } />
     </div>
     <div class="form-group">
       <div class="bootstrap-select-wrapper">
         <label for="select">Endpoint</label>
-        <select id="select" bind:value={action} title="Select an endpoint">
+        <select id="select" bind:value={action} title="Select an endpoing">
           <option value="util/send">Development (Local)</option>
           <option value="iosdk/send">Production</option>
         </select>
       </div>
     </div>
     <div class="form-group">
-      <button type="button" class="btn btn-primary" on:click={submitForm}>
+      <button type="button" class="btn btn-primary" disabled={!isFormValid} on:click={submitForm}>
         Send
       </button>
       <button type="button" class="btn btn-primary" on:click={resetForm}>
---
+++ admin/web/src/lib/validator/formValidator.js
@@ -0,0 +1,134 @@
+const RULES_SEPARATOR = '|'
+const RULE_PARAMETER_SEPARATOR = ':'
+
+const RULE_NUMERIC = 'numeric'
+const RULE_REQUIRED = 'required'
+const RULE_MAX_LENGTH = 'max'
+const RULE_MIN_LENGTH = 'min'
+const RULE_FISCALCODE = 'fiscalCode'
+
+const FIELD_VALUE_KEY = 'value'
+const FIELD_RULES_KEY = 'rules'
+
+/**
+ * Validates each form's field against a set of rule
+ * The field object should contain a value and a rules key
+ * 
+ * @param {array} fields 
+ * 
+ * @returns { object } validatedData
+ * 
+ * The return object contains a true/false as first property.
+ * As second property contains an the fields object, with the valid parameter foreach field.
+ * The second property can then be used by the frontend to display feedbacks or colors depending on the field validity.
+ * 
+ * 
+ */
+const validateForm = ( fields ) => {
+
+    let found = false;
+
+    Object.keys(fields).forEach((key, index) => {
+
+        const res = validateField( 
+                fields[key][FIELD_VALUE_KEY], 
+                fields[key][FIELD_RULES_KEY] 
+        );
+
+        fields[key].valid = res;
+
+        if(!res) {
+            found = true;
+        }
+            
+    })
+
+    return {
+        isFormValid: !found,
+        validatedData: fields
+    }
+
+}
+
+/**
+ * Validate a single value against a set of rules
+ * A set of rules is a "|" separated values
+ * 
+ * @param  { string } value
+ * @param  { string } rules
+ */
+const validateField = ( value, rules ) => {
+
+    if(rules === "") return true;
+
+    const rulesArray = explodeRules( rules );
+
+    return rulesArray.every( rule => validateFieldRule( value, rule ))
+
+}
+
+const explodeRules = ( rules ) => {
+
+    const rulesArray = rules.split(RULES_SEPARATOR)
+
+    return rulesArray.map( rule => rule.split(RULE_PARAMETER_SEPARATOR))
+
+}
+
+const validateFieldRule = ( field, rule ) => {
+
+    const ruleName = rule[0]
+    const ruleParam = rule[1]
+
+    switch( ruleName ) {
+
+        case(RULE_NUMERIC):
+            return validateNumericField( field )
+        case(RULE_FISCALCODE):
+            return validateFiscalCodeField( field )
+        case(RULE_MAX_LENGTH):
+            return validateMaxLengthField( field, ruleParam )
+        case(RULE_MIN_LENGTH):
+            return validateMinLengthField( field, ruleParam )
+        case(RULE_REQUIRED):
+            return validateRequiredField( field )
+        default:
+            return false
+            
+    }
+
+}
+
+const validateNumericField = ( value ) => {
+
+    return !isNaN( value )
+
+}
+
+const validateMaxLengthField = ( value, length ) => {
+
+    return value.length <= length
+
+}
+
+const validateMinLengthField = ( value, length ) => {
+
+    return value.length >= length
+
+}
+
+const validateRequiredField = ( value ) => {
+
+    return value.length > 0
+
+}
+
+const validateFiscalCodeField = ( value ) => {
+
+    const fiscalCodePattern = /^[a-zA-Z]{6}[0-9]{2}[a-zA-Z][0-9]{2}[a-zA-Z][0-9]{3}[a-zA-Z]$/
+
+    return value.search(fiscalCodePattern) !== -1
+
+}
+
+export const formValidator = { validateField, validateForm }    
